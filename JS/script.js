001a85b4f2f11c207af0ef6c0576101d29b9cc5c
saubhat10 <saurabhat10@gmail.com>
2022-02-09 11:21:58 +0530

    Final Update

--- JS/script.js
+++ JS/script.js
@@ -5,16 +5,35 @@ function calculateBMI() {
     var bmi = (weight / (heightM * heightM)).toFixed(2);
     document.getElementById("txt-bmi").innerHTML = "BMI: " + bmi;
 
-    if (bmi < 18.5) {
-        document.getElementById("txt-remark").innerHTML = "Under Weight";
-    } else if (bmi >= 18.5 && bmi <= 24.9) {
-        document.getElementById("txt-remark").innerHTML = "Normal";
+    if (bmi < 16) {
+        document.getElementById("txt-remark").innerHTML = "Serve Thickness ";
 
-    } else if (bmi >= 25 && bmi <= 29.9) {
+
+
+    } else if (bmi >= 16.1 && bmi <= 17) {
+        document.getElementById("txt-remark").innerHTML = "Modrate Thickness";
+
+
+
+    } else if (bmi >= 17.1 && bmi <= 18.4) {
+        document.getElementById("txt-remark").innerHTML = "Mild Thickness";
+
+
+    } else if (bmi > 18.5 && bmi <= 25) {
+        document.getElementById("txt-remark").innerHTML = "Normal ";
+
+    } else if (bmi >= 25.1 && bmi <= 29.9) {
         document.getElementById("txt-remark").innerHTML = "Over Weight";
 
-    } else if (bmi > 30) {
-        document.getElementById("txt-remark").innerHTML = "Obese";
+
+    } else if (bmi >= 30 && bmi <= 35) {
+        document.getElementById("txt-remark").innerHTML = "Obse Class I";
+
+    } else if (bmi >= 35.1 && bmi <= 40) {
+        document.getElementById("txt-remark").innerHTML = "Obse Class II";
+
+    } else if (bmi >= 41) {
+        document.getElementById("txt-remark").innerHTML = "Dead";
 
     }
 }
